a167d01d536a575c6e017bc8537eaae984c304d9
Vitalii <8imposible8@gmail.com>
2014-10-13 19:25:31 +0300

    All Lesson_3

--- Lesson_3_2/js/script.js
+++ Lesson_3_2/js/script.js
@@ -16,3 +16,25 @@ function updateClock () {
         format(d.getMinutes())+":"+format(d.getSeconds());
     clock.innerText = dateStr;
 }
+
+var R = 80,               // радиус окружности 
+            a = 0,                // угол (в радианах) 
+            da = 3*Math.PI/180,   // приращение угла 
+            delay = 30,           // задержка (в мсек.) 
+            x = 100, y = 100;
+        
+        // функция движения элемента 
+        function moveElem() {
+            // изменение координат элемента 
+            document.getElementById("elemToMove").style.left = x+R*Math.cos(a);
+            document.getElementById("elemToMove").style.top  = y+R*Math.sin(a);    
+            // увеличение расстояния до полюса 
+            a+=da;
+        }
+        // периодический вызов функции движения 
+        setInterval ("moveElem()", delay);
+
+        document.onmousemove = function(e) {
+            x = e.clientX;
+            y = e.clientY;
+        }
